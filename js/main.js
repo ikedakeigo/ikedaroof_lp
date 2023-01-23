a220479ce7d6ba85286d8c234af7f5c7b166023e
silverskg <chokichoki_gm@i.softbank.jp>
2023-01-23 10:08:32 +0900

    pcスタイル

--- js/main.js
+++ js/main.js
@@ -9,13 +9,33 @@
 //   }
 // }
 
-function toggleAnswer(element) {
-  var answer = element.nextElementSibling;
-  if (answer.style.display === "block") {
-    answer.style.display = "none";
-    element.innerHTML = "+";
+// function toggleAnswer(element) {
+//   var answer = element.nextElementSibling;
+//   if (answer.style.display === "block") {
+//     answer.style.display = "none";
+//     element.innerHTML = "+";
+//   } else {
+//     answer.style.display = "block";
+//     element.innerHTML = "-";
+//   }
+// }
+
+
+$(function () {
+  // タイトルをクリックすると
+  $(".js-accordion_title").on("click", function () {
+    // クリックした次の要素を開閉
+    $(this).next().slideToggle(300);
+    // タイトルにopenクラスを付け外しして矢印の向きを変更
+    $(this).toggleClass("open", 300);
+  });
+  });
+
+$('.inviewfadeInUp').on('inview', function (event, isInView) {
+  if (isInView) {
+    $(this).stop().addClass('fadeInUp');
   } else {
-    answer.style.display = "block";
-    element.innerHTML = "-";
+    //$(this).stop().removeClass('fadeInUp');
+    $(this).stop().removeClass('');
   }
-}
+});
